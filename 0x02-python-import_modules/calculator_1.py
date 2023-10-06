607fa281660493034282db95e874ef8084cdd00f
Esther Wanjiku <esther.wanjikum99@gmail.com>
2023-10-06 10:59:55 +0300

    1. My first toolbox!

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+        : first integer
+        : second integer
+
+    Returns:
+        he return value. a + b
+    """
+    return (a + b)
+
+
+def sub(a, b):
+    """My subtraction function
+
+    Args:
+        : first integer
+        : second integer
+    Returns:
+        he return value. a - b
+    """
+    return (a - b)
+
+
+def mul(a, b):
+    """My multiplication function
+
+    Args:
+        : first integer
+        : second integer
+
+    Returns:
+        he return value. a * b
+    """
+    return (a * b)
+
+
+def div(a, b):
+    """My division function
+
+    Args:
+        : first integer
+        : second integer
+
+    Returns:
+        he return value. a / b
+    """
+    return int(a / b)
