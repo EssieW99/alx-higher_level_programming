d52a52419a0236ddfeafc050945172673b3dc940
Esther Wanjiku <esther.wanjikum99@gmail.com>
2023-10-13 23:10:25 +0300

    13. Weighted average!

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/pyhton3
+def weight_average(my_list=[]):
+    total = 0
+    total_weight = 0
+    if not my_list:
+        return 0
+    for score, weight in my_list:
+        total += score * weight
+        total_weight += weight
+    weighted_average = total / total_weight
+    return weighted_average
