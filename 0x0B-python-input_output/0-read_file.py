8a91a45d492589b7009457d1424b74e1fd0bd48f
Esther Wanjiku <131749024+EssieW99@users.noreply.github.com>
2023-11-23 06:40:03 +0300

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/0-read_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""defines a text reading function"""
-
-
-def read_file(filename=""):
-    """reads a text file (UTF8) and prints it out"""
-    with open(filename, 'r', encoding='utf-8') as file:
-        for line in file:
-            print(line, end='')
--- 0x0B-python-input_output/4-from_json_string.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""defines a deserialization function"""
-import json
-
-
-def from_json_string(my_str):
-    """returns an object represented by a JSON string"""
-    return json.loads(my_str)
