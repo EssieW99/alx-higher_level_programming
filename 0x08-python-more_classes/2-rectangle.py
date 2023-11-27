3b7e95681efa2d05df68e67c55f69e709c70a555
Esther <esther.wanjikum99@gmail.com>
2023-11-27 13:15:07 +0300

    fix: typo error

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -22,7 +22,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """the setter method for the width property"""
-        if is not isinstance(value, int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -36,7 +36,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """the setter method for the height property"""
-        if is not isinstance(value, int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
