9d0be2b6dab16f1cfe963d0ab6edae84710ca874
Esther <esther.wanjikum99@gmail.com>
2023-12-05 05:20:42 +0300

    Correction

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -25,4 +25,4 @@ class Rectangle(BaseGeometry):
 
     def ___str__(self):
         """returns a string representation of the rectangle"""
-        return ("[Rectangle]: {}/{}".format(self.__width, self.__height))
+        return ("[Rectangle]: {}"/"{}".format(self.__width, self.__height))
