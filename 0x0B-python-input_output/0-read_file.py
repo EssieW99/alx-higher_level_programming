84337f834c0cf25c61d4da29ee11dbd070bbe8ab
Esther <esther.wanjikum99@gmail.com>
2023-12-05 17:08:59 +0300

    Add: documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""0-read_file module"""
+
 
 def read_file(filename=""):
+    """reads a txt file and prints it to stdout"""
     with open(filename, 'r', encoding="utf-8") as file:
         for line in file:
             print(line, end='')
