1b7c218fda7385586d3a476f225bf5f201c1c396
Esther <esther.wanjikum99@gmail.com>
2023-12-05 20:59:58 +0300

    Fix: pycodestyle error

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,4 +6,4 @@ import json
 def load_from_json_file(filename):
     """creates an object from a JSON file"""
     with open(filename, 'r') as file:
-      return  json.load(file)
+        return json.load(file)
