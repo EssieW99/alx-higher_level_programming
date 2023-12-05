bb8bfc696a2e9efb995c7025a159ccac1eff4f39
Esther <esther.wanjikum99@gmail.com>
2023-12-05 20:58:32 +0300

    Fix: error

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,4 +6,4 @@ import json
 def load_from_json_file(filename):
     """creates an object from a JSON file"""
     with open(filename, 'r') as file:
-        json.load(file)
+      return  json.load(file)
