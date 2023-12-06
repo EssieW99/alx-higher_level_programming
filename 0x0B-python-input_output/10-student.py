fb8c1ecfee7042b373230acb18a90c08662aec88
Esther <esther.wanjikum99@gmail.com>
2023-12-06 04:55:04 +0300

    10. Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""defines a class"""
+
+
+class Student:
+    """a class on a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """initialises a new instance of the class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns a dictionary representation of a Student instance
+
+        Args:
+        attrs: a list of string only attribute names contained in this list
+        must be obtained, otherwise all must be retrived
+
+        Returns:
+        a dictionary containing the selected attributes of the instance
+        """
+        if attrs is None:
+            return self.__dict__
+        selected_attr = {
+                key: value
+                for key, value in self.__dict__.items()
+                if key in attrs
+        }
+        return selected_attr
