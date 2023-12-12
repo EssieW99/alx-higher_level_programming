6fe5c050ff3fe1607ac1152cb6471f39d8090ce5
Esther <esther.wanjikum99@gmail.com>
2023-12-12 04:36:04 +0300

    fix: typo error

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -53,12 +53,12 @@ class Base:
             return []
         return json.loads(json_string)
 
-    @classicmethod
+    @classmethod
     def create(cls, **dictionary):
         """returns an instance with all attributes
         already set
         """
-        if cls.__name__ = "Rectangle":
+        if cls.__name__ == "Rectangle":
             dummy_instance = cls(1, 1)
         else:
             dummy_instance = cls(1)
