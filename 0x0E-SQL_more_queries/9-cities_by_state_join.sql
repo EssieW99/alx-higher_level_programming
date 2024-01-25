ee9824c28603a22d13d6238c11244828a1df14c8
Esther <esther.wanjikum99@gmail.com>
2024-01-25 14:37:51 +0300

    9. Cities by States

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- Write a script that lists all cities contained in the database hbtn_0d_usa.
+
+SELECT cities.name, cities.id, states.name
+FROM cities
+INNER JOIN states ON cities.id = states.id
+ORDER BY id ASC;
