c772e4dda09631aa388f3ff498f33a455b9c5331
Esther <esther.wanjikum99@gmail.com>
2024-02-14 11:04:58 +0300

    Fix: Error

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,7 +3,7 @@
 
 exports.esrever = function (list) {
   const l = [];
-  for (let i = list.length - 1; i > 0; i--) {
+  for (let i = list.length - 1; i > -1; i--) {
     l.push(list[i]);
   }
   return (l);
