821bcd1256ab9155954c3f35abe63fe5438314e6
Esther <esther.wanjikum99@gmail.com>
2024-02-14 19:11:11 +0300

    9. Log me

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -2,7 +2,8 @@
 // a function that prints the number of args already printed and the new args value
 
 let count = 0;
+
 exports.logMe = function (item) {
-  console.log(count + ' : ' + item);
+  console.log(count + ': ' + item);
   count++;
 };
