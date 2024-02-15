5c94b72ee48e0e742e005ed7af2a4077e4293102
Esther <esther.wanjikum99@gmail.com>
2024-02-15 16:14:42 +0300

    13. Concat files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// a script that concats 2 files
+
+// import fs module that provides functions for working with the file system
+const fs = require('fs');
+
+// get contents in first and second source file
+const firstFile = fs.readFileSync(process.argv[2]).toString();
+const secFile = fs.readFileSync(process.argv[3]).toString();
+
+// write contents into file three
+fs.writeFileSync(process.argv[4], firstFile + '\n' + secFile + '\n');
