9079e572774f6c2f17f9dedf298c1ce7cf4509b6
Esther Wanjiku <esther.wanjikum99@gmail.com>
2024-02-18 11:42:50 +0300

    0. Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.10
 """ a script that lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
@@ -13,12 +13,6 @@ Close cursor and connection
 """
 
 if __name__ == "__main__":
-    print("Connecting to MySQL database...")
-    print("Host:", "localhost")
-    print("User:", sys.argv[1])
-    print("Password:", sys.argv[2])
-    print("Database:", sys.argv[3])
-
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
