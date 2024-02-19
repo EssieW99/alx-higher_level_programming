6efea8e11041d02c29977947705c9f909e208f71
Esther Wanjiku <esther.wanjikum99@gmail.com>
2024-02-19 13:47:30 +0300

    14. Cities in state

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+script that prints all City objects from the database
+"""
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+    """ create engine"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    """ create a session"""
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(City, State).join(State)
+    for c, s in query.all():
+        print("{}: {} {}".format(s.name, c.id, c.name))
+
+    session.commit()
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+contains the class definition of a City
+"""
+from model_state import Base, State
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+class City(Base):
+    """
+    a class on cities that inherits the Base class
+    """
+
+    __tablename__ = 'cities'
+
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
