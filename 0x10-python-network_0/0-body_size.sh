cff52fa31915b1a3c6524e5bfcbe21544f06cfbc
Esther Wanjiku <esther.wanjikum99@gmail.com>
2024-03-01 00:52:00 +0300

    Corrections

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a script that sends a request to a URL and displays the size of the body
-curl -sI "$1" | grep -i "Content-Length" | cut -d " " -f2
+curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
